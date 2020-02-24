96528526d61ff8274c42b6cc2b6f30a9300158b5
Steve <stevekentlanier@gmail.com>
2020-02-24 11:44:11 -0800

    added Bubl project details

--- js/scrollfade.js
+++ js/scrollfade.js
@@ -1,8 +1,8 @@
 $(window).scroll(function(){
-    $(".scrollfade").css("opacity", 1 - $(window).scrollTop() / 550);
+    $(".scrollfade").css("opacity", 1 - $(window).scrollTop() / 750);
     console.log(scroll);
 });
 $(window).scroll(function(){
-    $(".masthead").css("opacity", 1 - $(window).scrollTop() / 550);
+    $(".masthead").css("opacity", 1 - $(window).scrollTop() / 1000);
     console.log(scroll);
 });
